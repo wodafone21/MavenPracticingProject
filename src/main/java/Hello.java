b0f72fa3fb2317b57376fdcfb02b939499a6d4de
A Alver <wodafone@hotmail.com>
2021-09-18 06:25:07 +0100

    added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing hello feature");
+        System.out.println("Local Repo is already published in Github");
+        System.out.println("doin more work");
     }
 }
